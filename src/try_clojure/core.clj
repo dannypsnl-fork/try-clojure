b367a56e55f086659b68d29e8efcdc47e48a1254
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-04-01 01:14:56 +0800

    multiple method definition

--- src/try_clojure/core.clj
+++ src/try_clojure/core.clj
@@ -2,3 +2,8 @@
 
 (defn id "I return what I get"
   [x] x)
+
+(defn multi-add
+  ([a b c] (+ a b c))
+  ([a b] (+ a b))
+  ([a] a))
--- test/try_clojure/core_test.clj
+++ test/try_clojure/core_test.clj
@@ -5,3 +5,9 @@
 (deftest id-test
   (testing "id returns what it get"
     (is (= (id 0) 0))))
+
+(deftest multi-add-test
+  (testing "multi-add"
+    (is (= (multi-add 1 2 3) 6))
+    (is (= (multi-add 1 2) 3))
+    (is (= (multi-add 1) 1))))
